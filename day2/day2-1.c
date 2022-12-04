4a80bd329864961663fc662614e7cf79970adf10
Nat H <74020864+MxSelfDestruct@users.noreply.github.com>
2022-12-03 22:05:51 -0800

    weeeeeeeeeee

---
+++ day2/day2-1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define BUF_LEN 8
+
+unsigned int eval_rochambeau(char xyz, char abc);
+
+int main(void) {
+    FILE * data = fopen("input.txt", "r");
+
+    unsigned int score = 0;
+    char buf[BUF_LEN] = {'\0'};
+
+    while (fgets(buf, BUF_LEN, data) != NULL) {
+        score += eval_rochambeau(buf[2], buf[0]);
+    }
+
+    printf("Final score: %u\n", score);
+
+    fclose(data);
+
+    return 0;
+}
+
+unsigned int eval_rochambeau(char xyz, char abc) {
+    unsigned int score = xyz - 'X' + 1;
+    
+    if (xyz == 'X' && abc == 'C') score += 6; // Win
+    if (xyz == 'X' && abc == 'A') score += 3; // Draw
+    if (xyz == 'X' && abc == 'B') score += 0; // Loss
+
+    if (xyz == 'Y' && abc == 'A') score += 6;
+    if (xyz == 'Y' && abc == 'B') score += 3;
+    if (xyz == 'Y' && abc == 'C') score += 0;
+
+    if (xyz == 'Z' && abc == 'B') score += 6;
+    if (xyz == 'Z' && abc == 'C') score += 3;
+    if (xyz == 'Z' && abc == 'A') score += 0;
+
+    return score;
+}
---
+++ day3/day3-1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUF_LEN 64
+
+unsigned int get_priority(char item);
+void copy_str_span(char * target, char * source, unsigned int start, unsigned int end);
+void get_common_chars(char * target, char * source1, char * source2);
+
+int main(void) {
+    FILE * data = fopen("input.txt", "r");
+
+    char buf[BUF_LEN] = {'\0'};
+    unsigned int priority_sum = 0;
+
+    while(fgets(buf, BUF_LEN, data) != NULL) {
+        buf[strcspn(buf, "\n")] = '\0'; // Send trailing newline to hell
+        unsigned int len = strlen(buf);
+
+        // Split string into halves and find the letters they have in common
+        char bag1[BUF_LEN] = {'\0'};
+        char bag2[BUF_LEN] = {'\0'};
+        char common[BUF_LEN] = {'\0'};
+
+        copy_str_span(bag1, buf, 0, len / 2 - 1);
+        copy_str_span(bag2, buf, len / 2, len);
+        get_common_chars(common, bag1, bag2);
+
+        for (int i = 0; i < strlen(common); i++) priority_sum += get_priority(common[i]);
+    }
+
+    printf("%u\n", priority_sum);
+
+    fclose(data);
+
+    return 0;
+}
+
+unsigned int get_priority(char item) {
+    if (item >= 'a' && item <= 'z') return item - 'a' + 1;
+    if (item >= 'A' && item <= 'Z') return item - 'A' + 27;
+
+    return 0;
+}
+
+void copy_str_span(char * target, char * source, unsigned int start, unsigned int end) {
+    for (int i = 0; i <= end; i++) {
+        target[i] = source[i + start];
+
+        if (i == end) {
+            if (target[i] != '\0') target[i + 1] = '\0';
+        }
+    }
+}
+
+void get_common_chars(char * target, char * source1, char * source2) {
+    unsigned int freq1[128] = {0};
+    unsigned int freq2[128] = {0};
+
+    for (int i = 0; i < strlen(source1); i++) freq1[source1[i]]++;
+    for (int i = 0; i < strlen(source2); i++) freq2[source2[i]]++;
+
+    unsigned int target_index = 0;
+
+    for (int i = 0; i < 128; i++) {
+        if (freq1[i] > 0 && freq2[i] > 0) {
+            target[target_index] = i;
+            target_index++;
+        }
+    }
+
+    for (int i = target_index; i < strlen(target); i++) target[i] = '\0';
+}
