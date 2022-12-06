1725463c229032aa9a352479790c41520a5bcc9e
Nat H <74020864+MxSelfDestruct@users.noreply.github.com>
2022-12-05 21:57:08 -0800

    wait functions

--- day6/day6.c
+++ day6/day6.c
@@ -4,11 +4,10 @@
 
 #define BUF_LEN 8192
 #define SLICE_LEN 16
-#define PACKET_MARKER_LEN 4
-#define MESSAGE_MARKER_LEN 14
 
 void get_substring(char * target, const char * source, unsigned int start, unsigned int end);
-bool letters_unique(char * str);
+bool letters_unique(const char * str);
+unsigned int get_start_marker(const char * str, unsigned int unique_chars_needed);
 
 int main(void) {
     FILE * data = fopen("input.txt", "r");
@@ -16,29 +15,8 @@ int main(void) {
     char buf[BUF_LEN] = {'\0'};
     fgets(buf, BUF_LEN, data);
 
-    char slice[SLICE_LEN] = {'\0'};
-
-    // Get start-of-packet marker
-    for (int i = 0; i < strlen(buf) - PACKET_MARKER_LEN; i++) {
-        get_substring(slice, buf, i, i + PACKET_MARKER_LEN - 1);
-
-        if (letters_unique(slice) == true) {
-            printf("Start-of-packet marker: %c", slice[strlen(slice) - 1]);
-            printf(", after %u characters.\n", i + PACKET_MARKER_LEN);
-            break;
-        }
-    }
-
-    // Get start-of-message marker
-    for (int i = 0; i < strlen(buf) - MESSAGE_MARKER_LEN; i++) {
-        get_substring(slice, buf, i, i + MESSAGE_MARKER_LEN - 1);
-
-        if (letters_unique(slice) == true) {
-            printf("Start-of-message marker: %c", slice[strlen(slice) - 1]);
-            printf(", after %u characters.\n", i + MESSAGE_MARKER_LEN);
-            break;
-        }
-    }
+    printf("Start-of-packet marker detected after %u characters.\n", get_start_marker(buf, 4));
+    printf("Start-of-message marker detected after %u characters.\n", get_start_marker(buf, 14));
 
     return 0;
 }
@@ -47,10 +25,22 @@ void get_substring(char * target, const char * source, unsigned int start, unsig
     for (unsigned int i = start; i <= end; i++) target[i - start] = source[i];
 }
 
-bool letters_unique(char * str) {
+bool letters_unique(const char * str) {
     unsigned int freq[128] = {0};
     for (unsigned int i = 0; i < strlen(str); i++) freq[str[i]]++;
     for (unsigned int i = 0; i < 128; i++) if (freq[i] > 1) return false;
 
     return true;
+}
+
+unsigned int get_start_marker(const char * str, unsigned int unique_chars_needed) {
+    char slice[SLICE_LEN] = {'\0'};
+
+    for (int i = 0; i < strlen(str) - unique_chars_needed; i++) {
+        get_substring(slice, str, i, i + unique_chars_needed - 1);
+
+        if (letters_unique(slice) == true) {
+            return i + unique_chars_needed;
+        }
+    }
 }
